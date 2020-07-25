a371ecec152c6e5f67719354a58fc08e9f75e842
Anthony <anthonytony2000.04@gmail.com>
2020-07-25 11:04:45 -0500

    add passwGen.js, that is the function that do the magic

---
+++ passwGen.js
@@ -0,0 +1,11 @@
+
+exports.randomPass =()=>{
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-+=\|/{}[]:?;.,0123456789';
+    var password = "";
+    for (var i = 0, n = charset.length; i < 8; i++) {
+        password += charset.charAt(Math.floor(Math.random() * Math.random() * n));
+
+    }
+   return password;
+}
+
